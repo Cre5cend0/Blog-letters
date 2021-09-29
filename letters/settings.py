cfed132632537560b7ea183483ca85bd45a30deb
Manish Raj <60720070+Cre5cend0@users.noreply.github.com>
2021-09-29 19:42:59 +0530

    changing settings

--- letters/settings.py
+++ letters/settings.py
@@ -19,7 +19,6 @@ from django.core.management.utils import get_random_secret_key
 PRODUCTION_KEY = get_random_secret_key()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -27,14 +26,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ.get('LETTERS_APP_SECRET_KEY', 'django-insecure-n==dcfp#6x9!i*skoy5(0jln+!1z^hvv_140p*v^fbn4i+0hr8')
-# SECRET_KEY = 'django-insecure-n==dcfp#6x9!i*skoy5(0jln+!1z^hvv_140p*v^fbn4i+0hr8'
-SECRET_KEY = PRODUCTION_KEY
+SECRET_KEY = os.environ.get('LETTERS_APP_SECRET_KEY', 'django-insecure-n==dcfp#6x9!i*skoy5(0jln+!1z^hvv_140p*v^fbn4i+0hr8')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-# DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 
 ALLOWED_HOSTS = ['127.0.0.1', 'blog-letters.herokuapp.com']
